eb184ce9d13ec912b3ccd6a9737cd8eec7e7f788
Nusratillo Nematillayev <nusratillo707@gmail.com>
2020-04-27 11:55:59 +0500

    Longest Common Subsequence problem solution added

---
+++ src/LongestCommonSubsequence.kt
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/longest-common-subsequence/
+
+import kotlin.math.max
+
+class LongestCommonSubsequence {
+
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        val N = max(text1.length, text2.length)
+        val dp = arrayOf(
+            IntArray(N) { 0 },
+            IntArray(N) { 0 }
+        )
+        text1.forEachIndexed { index1, c1 ->
+            var MX = 0
+            text2.forEachIndexed { index2, c2 ->
+                if (c1 == c2) {
+                    dp[index1 % 2][index2] = MX + 1
+                }
+                MX = max(MX, dp[(index1 + 1) % 2][index2])
+                dp[index1 % 2][index2] = max(dp[(index1 + 1) % 2][index2], dp[index1 % 2][index2])
+            }
+            for (i in 0 until N)
+                print("${dp[index1 % 2][i]} ")
+            println()
+        }
+        var ans = 0
+        for (i in 0 until N) {
+            ans = max(ans, dp[(text1.length - 1) % 2][i])
+        }
+        return ans
+    }
+}
+
+fun main() {
+    val longestCommonSubsequence = LongestCommonSubsequence()
+    println(longestCommonSubsequence.longestCommonSubsequence("abc", "def"))
+}
