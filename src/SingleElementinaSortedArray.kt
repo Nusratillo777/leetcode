8d65d460c58ec41275c9818e78de2b170761a2cf
Nusratillo Nematillayev <nusratillo707@gmail.com>
2020-05-12 13:01:32 +0500

    Single Element in a Sorted Array problem solution added

--- src/SingleElementinaSortedArray.kt
+++ src/SingleElementinaSortedArray.kt
@@ -3,7 +3,6 @@ class SingleElementinaSortedArray {
         var l = 0
         var r = nums.size - 1
         while (l < r) {
-            println("$l $r")
             val mid = (l + r) / 2
             if (mid + 1 < nums.size && nums[mid] == nums[mid + 1]) {
                 if ((mid - l) % 2 == 1)
@@ -21,14 +20,4 @@ class SingleElementinaSortedArray {
         }
         return nums[r]
     }
-}
-
-fun main() {
-    val singleElementinaSortedArray = SingleElementinaSortedArray()
-    println(singleElementinaSortedArray.singleNonDuplicate(intArrayOf(1, 1, 2, 3, 3, 4, 4, 8, 8)))
-    println(singleElementinaSortedArray.singleNonDuplicate(intArrayOf(3, 3, 7, 7, 10, 11, 11)))
-    println(singleElementinaSortedArray.singleNonDuplicate(intArrayOf(3, 3, 7, 7, 10, 10, 11, 11, 12)))
-    println(singleElementinaSortedArray.singleNonDuplicate(intArrayOf(3, 3, 7)))
-    println(singleElementinaSortedArray.singleNonDuplicate(intArrayOf(1, 3, 3)))
-    println(singleElementinaSortedArray.singleNonDuplicate(intArrayOf(1)))
 }
